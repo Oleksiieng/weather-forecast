d9990aae71405a0e2e868504a9b76984218aba3a
Oleksii Kulykov <oleksii.eng@gmail.com>
2024-01-16 18:03:36 +0000

    added method fetchWeatherForecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,39 @@ const apiKey = 'c5c492606c2d753c67314dbe344b9f50';
 
 const city = 'London';
 
+function fetchWeatherForecast(lat, lon) {
+    const forecastApiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(forecastApiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather forecast:', error);
+        });
+}
+
+
 function getCoordinatesForCity(cityName) {
     const geocodingApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
 
     fetch(geocodingApiUrl)
-      .then(response => response.json())
-      .then(data => {
-        if (data && data.length > 0) {
-          const { lat, lon } = data[0];
-          console.log(`${cityName}: Lat ${lat}, Lon ${lon}`);
-         
-        } else {
-          console.log(`No coordinates found for the specified city. ${cityName}`);
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching coordinates:', error);
-      });
-  }
-  
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.length > 0) {
+                const { lat, lon } = data[0];
+                console.log(`${cityName}: Lat ${lat}, Lon ${lon}`);
 
-  getCoordinatesForCity(city);
+                fetchWeatherForecast(lat, lon);
+            } else {
+                console.log(`No coordinates found for the specified city. ${cityName}`);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching coordinates:', error);
+        });
+}
+
+
+getCoordinatesForCity(city);
